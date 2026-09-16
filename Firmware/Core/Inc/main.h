7d89a16301d1a820b2187a7ef3eef54d440cc926
nicola.borkowetz <nborkowetz@gmail.com>
2026-09-16 22:13:52 +0200

    fix: use MSI for system and RNG clocks

--- Firmware/Core/Inc/main.h
+++ Firmware/Core/Inc/main.h
@@ -29,8 +29,6 @@ extern "C" {
 /* Includes ------------------------------------------------------------------*/
 #include "stm32wlxx_hal.h"
 
-#include "stm32wlxx_nucleo.h"
-
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
